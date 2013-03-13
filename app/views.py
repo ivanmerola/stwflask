98a1ce9d653e38f16ae1f52c693b57ba08e2a094
user06 <user06@alcatel13.(none)>
2013-03-13 12:42:41 +0100

    changed programs

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-	return "Nothing here yet!"
+	user = { 'name' : 'Barcenas', 'city' : 'Madrid' }
+	sobres =[{
+			'company' : { 'name' : 'ACS' },
+			'Amount' : '100.000EUR'
+		 },
+		 {
+			'company' : { 'name' : 'FCC' },
+			'Amount' : '150.000EUR'
+		 },
+		]
+	return render_template("index.html",
+		pagetitle = 'Home',
+		user = user, envelopes = sobres)
